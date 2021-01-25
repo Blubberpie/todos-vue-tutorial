18eafa60108a9e247af5af5d069671a41ef9c540
Zwel Pai <lozberipie@gmail.com>
2021-01-25 21:48:39 +0700

    Done local storage

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const LOCAL_STORAGE_KEY = 'todo-app-vue';
+
 const todoApp = new Vue({
   el: '.todoapp',
   data: {
     title: 'Todos',
-    todos: [
+    todos: JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [
       { text: 'Learn JavaScript ES6+ goodies', isDone: true },
       { text: 'Learn Vue', isDone: false },
       { text: 'Build something awesome', isDone: false },
@@ -12,7 +14,7 @@ const todoApp = new Vue({
   methods: {
     createTodo(event) {
       const textbox = event.target;
-      this.todos.push({ text: textbox.value, isDone: false });
+      this.todos.push({ text: textbox.value.trim(), isDone: false });
       textbox.value = '';
     },
     startEditing(todo) {
@@ -21,11 +23,34 @@ const todoApp = new Vue({
     finishEditing(event) {
       if (!this.editing) { return; }
       const textbox = event.target;
-      this.editing.text = textbox.value;
+      this.editing.text = textbox.value.trim();
       this.editing = null;
     },
     cancelEditing() {
       this.editing = null;
+    },
+    destroyTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    clearCompleted() {
+      this.todos = this.activeTodos;
+    },
+  },
+  computed: {
+    activeTodos() {
+      return this.todos.filter(t => !t.isDone);
+    },
+    completedTodos() {
+      return this.todos.filter(t => t.isDone);
     }
-  }
+  },
+  watch: {
+    todos: {
+      deep: true,
+      handler(newValue) {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newValue));
+      }
+    }
+  },
 });
